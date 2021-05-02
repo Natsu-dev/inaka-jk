9519b99c625ae5e3681563235853e9f570288b5b
Natsu-Dev <Natsu-aoiro@s.okayama-u.ac.jp>
2021-05-03 00:49:31 +0900

    % command pre

--- index.js
+++ index.js
@@ -15,6 +15,8 @@ client.on("ready", () => {
 
 client.on("guildCreate", guild => {
 
+    var phrases = require("./phrases.js");
+
     let greetingChannel = "";
     guild.channels.cache.forEach((channel) => {
         if (channel.type == "text" && greetingChannel == "") {
@@ -23,11 +25,13 @@ client.on("guildCreate", guild => {
             }
         }
     })
-    greetingChannel.send("やっほー！　情クラ＠田舎鯖の稼働状況を伝えるbot Inaka-jk だよ！　よろしくね！");
+    greetingChannel.send(phrases.guildCreateGreeting);
 });
 
 // メッセージ受け取り時
 client.on("message", message => {
+
+    var phrases = require("./phrases.js");
 
     // 自分のだったら無視
     if (message.author.bot) {
@@ -40,13 +44,21 @@ client.on("message", message => {
     // おはよう
     if (message.content.match(/おはよ/)) {
 
-        message.channel.send("おはよー！") //メッセ送信
-            .then(message => console.log(`Sent message: ${reply_text} to ${author}`)) // コンソールにも出す
+        message.channel.send(phrases.goodMorning) //メッセ送信
+            .then(message => console.log(`Sent message: ${phrases.goodMorning} to ${author}`)) // コンソールにも出す
             .catch(console.error);
         return;
     }
 
-});
+    // コマンド系
+    if (message.content.startsWith("%")) {
 
+        var i = 0;
+        for (i = 0; message.content.substr(1).charAt(i) == " "; i++); // % の続きについて空白文字でなくなるまで i を加算
+        var commandArray = message.content.substr(i + 1).split(/\s+/); // 空白でない部分からを任意長の空白で区切る
+
+        message.channel.send(commandArray[0]+commandArray[1]);
+    }
+});
 
 client.login(process.env.DISCORD_TOKEN);
---
+++ phrases.js
@@ -0,0 +1,6 @@
+// guildCreate
+exports.guildCreateGreeting = "やっほー！　情クラ＠田舎鯖の稼働状況を伝えるbot Inaka-jk だよ！　よろしくね！";
+
+// greetings
+exports.goodMorning = "おはよー！";
+//exports.
