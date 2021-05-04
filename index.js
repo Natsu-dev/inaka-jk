7be044a2047b0116ea590270c2f49655c922a499
Natsu-Dev <Natsu-aoiro@s.okayama-u.ac.jp>
2021-05-04 23:56:47 +0900

    restart command updated

--- index.js
+++ index.js
@@ -1,4 +1,7 @@
-require('dotenv').config(); // 環境変数に.env使う
+const path = require('path');
+var fs = require('fs');
+require('dotenv').config({ path: path.join(__dirname, '.env') }); // 環境変数に.env使う
+
 var phrases = require("./phrases.js"); // JK語
 
 // ログイン処理
@@ -11,6 +14,27 @@ client.on("ready", () => {
         // type: "STREAMING", //PLAYING: WATCHING: LISTENING: STREAMING: // unused?
         //url: "" }
     );
+    if (fs.existsSync("manual-restart.json")) {
+
+        var data = JSON.parse(fs.readFileSync("manual-restart.json", 'utf8'));
+
+        time = new Date(data.time);
+        nowtime = new Date();
+
+        // 5分以内に再起動が行われていてかつ再起動が未完了のとき
+        if (nowtime <= Date(time.setMinutes(Date(time.getMinutes()) + 5)) && data.finished === false) {
+
+            data.finished = true;
+
+            data.channel.send(phrases.afterRestart)
+                .then(message => console.log(`Restart done.`))
+                .catch(console.error);
+
+            fs.writeFileSync("manual-restart.json", JSON.stringify(data, null, 4))
+                .then(console.log("'manual-restart.json' updated.")); // 再起動完了を記録
+        }
+    }
+
     console.log("ready...");
 });
 
@@ -42,7 +66,7 @@ client.on("message", message => {
     if (message.content.match(/おはよ/)) {
 
         message.channel.send(phrases.goodMorning) //メッセ送信
-            .then(message => console.log(`Sent message: ${phrases.goodMorning} to ${author}`)) // コンソールにも出す
+            .then(message => console.log(`Message: ${phrases.goodMorning} to ${author}.`)) // コンソールにも出す
             .catch(console.error);
         return;
     }
@@ -56,14 +80,27 @@ client.on("message", message => {
 
         if (commandArray[0].match("restart")) { // restartコマンド: サーバーの再起動を実行
 
-            message.channel.send("```INFO: restarting server...```")
+            var mrlog = {
+                time: new Date(),
+                finished: false,
+                channel: message.channel
+            };
+            fs.writeFileSync("manual-restart.json", JSON.stringify(mrlog, null, 4)); // 手動再起動の完了を記録
+            console.log("'manual-restart.json' saved.");
+
+            message.channel.send(phrases.beforeRestart)
+                .then(message => console.log(`Restart message.`))
+                .catch(console.error);
 
             const exec = require('child_process').exec;
-
             exec(process.env.RESTART_BASH, function (err, stdout, stderr) {
-                if (stderr) message.channel.send(phrases.restartFail + "\n```" + stderr + "```")
-                else message.channel.send(phrases.restartSuccess)
+                if (err) {
+                    console.log(`stderr: ${stderr}`)
+                    return
+                }
+                console.log(`stdout: ${stdout}`)
             });
+            return;
         }
 
         // マジで何でも出来ちゃうっぽいので一旦削除
@@ -75,6 +112,8 @@ client.on("message", message => {
         // 
         //     message.channel.send(execSync(bash.join(" ")).toString());
         // }
+
+        return;
     }
 });
 
--- phrases.js
+++ phrases.js
@@ -2,11 +2,11 @@
 exports.activity = "情クラ＠田舎鯖"
 
 // guildCreate
-exports.guildCreateGreeting = "やっほー！　情クラ＠田舎鯖の稼働状況を伝えるbot Inaka-jk だよ！　よろしくね！";
+exports.guildCreateGreeting = "やっほー！　情クラ＠田舎鯖の管理bot Inaka-jk だよ！　よろしくね！";
 
 // greetings
 exports.goodMorning = "おはよー！";
 
 // commands
-exports.restartFail = "ごっめーん、再起動できなかった……\nエラーでたから置いとくね……";
-exports.restartSuccess = "おっけー！サーバーを再起動するよ！";
+exports.beforeRestart = "おっけー！　サーバーを再起動するよ！";
+exports.afterRestart = "サーバーの再起動ができたよ！　褒めてー！"
